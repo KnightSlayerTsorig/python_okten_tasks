a3031c913050efd9173e052012525c3cdbed92dc
Oleg Pilyak <olkooop@gmail.com>
2021-02-19 14:55:25 +0200

    Fixed some things

--- home_task_1.py
+++ home_task_1.py
@@ -9,19 +9,12 @@ def minimal(arr):
 
 # удалить все одинаковые значения
 def delete_similar(arr):
-    new_arr = arr
     new_numbers = []
-    for el in new_arr:
+    for el in arr:
         if el not in new_numbers:
             new_numbers.append(el)
-    for el in new_numbers:
-        if el in new_arr:
-            new_arr.remove(el)
-    for el in new_arr:
-        if el in new_numbers:
-            new_numbers.remove(el)
     print(new_numbers)
-    print('Значения ' + str(arr) + ' были временно удалены')
+    print('Дубликаты были временно удалены')
 
 
 # заменить каждое четвертое значение на "Х"
@@ -40,16 +33,25 @@ def find_avg(arr):
     divider = 0
     for el in arr:
         if type(el) == int:
-            sum_of_elements += el
-            divider += 1
+            if el > 0:
+                sum_of_elements += el
+                divider += 1
     avg = sum_of_elements / divider
-    avg_in_list = arr[0]
+    avg_in_list_dif = sum_of_elements
+    avg_in_list = 0
     for el in arr:
-        if type(el) == int:
-            if avg == el:
+        if el > avg:
+            diff = el - avg
+            if diff < avg_in_list_dif:
+                avg_in_list_dif = diff
                 avg_in_list = el
-            elif avg - el < avg_in_list:
+        elif avg > el:
+            diff = avg - el
+            if diff < avg_in_list_dif:
+                avg_in_list_dif = diff
                 avg_in_list = el
+        else:
+            avg_in_list = el
     print(avg_in_list)
 
 
@@ -72,53 +74,67 @@ def table():
     multiplier_2 = 1
     while multiplier_1 != 10:
         while multiplier_2 != 10:
-            print(str(multiplier_1 * multiplier_2) + ' ', end="")
+            a = multiplier_1 * multiplier_2
+            if a < 10:
+                print(' ' + str(a) + ' ', end="")
+            else:
+                print(str(a) + ' ', end="")
             multiplier_2 += 1
-        print('\n')
+        print(' ')
         multiplier_1 += 1
         multiplier_2 = 1
 
-
+        
 # переделать первое задание под меню с помощью цикла
-while True:
-    numbers = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
-    print('Что будем делать?')
-    print('Введите 1 что бы найти min число в листе ')
-    print('Введите 2 что бы удалить все одинаковые значения')
-    print('Введите 3 что бы заменить каждое четвертое значение на "Х"')
-    print(
-        '''Введите 4 что бы вывести элемент листа, значение которого ближе
-всего к среднему арифметическому всех элементов этого же листа'''
-    )
-    print('Введите 5 что бы увидить магический квадрат')
-    print('Введите 6 что бы увидить табличку умножения ')
-    print('Введите 7 что бы выйти з программы')
-    action = input('Введите число: ')
-    action_verification = [str(el) for el in range(1, 8)]
-    if action not in action_verification:
-        print('Вы ввели неправильное число или вообще не число, не надо так!')
-    else:
-        action = int(action)
-        if action == 1:
-            minimal(numbers)
-        if action == 2:
-            delete_similar(numbers)
-        if action == 3:
-            change_4_for_x(numbers)
-        if action == 4:
-            find_avg(numbers)
-        if action == 5:
-            h = int(input('Введите желаемую высоту магического квадрата: '))
-            w = int(input('Введите желаемую  ширину магического квадрата: '))
-            if type(h) != int or type(w) != int:
-                print('Ну вот опять... вот вам базовый квадрат')
-                stars(6, 6)
-            else:
-                stars(h, w)
-            if h != w:
-                print('Только это не квадрат а прямоугольник')
-        if action == 6:
-            table()
-        if action == 7:
-            break
+numbers1 = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+numbers2 = [-1, -2, 3, 4, 555]
+numbers3 = [5, 5, 5, 5]
+numbers4 = [-10, 5, 5]
+
+
+def menu(arr):
+    while True:
+        print('Что будем делать?')
+        print('Введите 1 что бы найти min число в листе ')
+        print('Введите 2 что бы удалить все одинаковые значения')
+        print('Введите 3 что бы заменить каждое четвертое значение на "Х"')
+        print(
+            '''Введите 4 что бы вывести элемент листа, значение которого ближе
+    всего к среднему арифметическому всех элементов этого же листа'''
+        )
+        print('Введите 5 что бы увидить магический квадрат')
+        print('Введите 6 что бы увидить табличку умножения ')
+        print('Введите 7 что бы выйти з программы')
+        action = input('Введите число: ')
+        action_verification = ['1', '2', '3', '4', '5', '6', '7']
+        if action not in action_verification:
+            print('Вы ввели неправильное число или вообще не число, не надо так!')
+        else:
+            action = int(action)
+            if action == 1:
+                minimal(arr)
+            if action == 2:
+                delete_similar(arr)
+            if action == 3:
+                change_4_for_x(arr)
+            if action == 4:
+                find_avg(arr)
+            if action == 5:
+                h = int(input('Введите желаемую высоту магического квадрата: '))
+                w = int(input('Введите желаемую  ширину магического квадрата: '))
+                if type(h) != int or type(w) != int:
+                    print('Ну вот опять... вот вам базовый квадрат')
+                    stars(6, 6)
+                else:
+                    stars(h, w)
+                if h != w:
+                    print('Только это не квадрат а прямоугольник')
+            if action == 6:
+                table()
+            if action == 7:
+                break
+
+
+menu(numbers1)
+
 
